c2df80a63e4cbc2bf3ba9e85747bd72267f30f0e
Szymon Cybulski <szymon9@windowslive.com>
2024-01-17 23:49:21 +0100

    phpunit

---
+++ tests/ExampleTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @test
+     */
+    public function trueIsTrue(): void
+    {
+        $this->assertTrue(true);
+    }
+}
